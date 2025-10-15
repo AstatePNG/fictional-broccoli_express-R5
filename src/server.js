a71e8f664986ec6e819631b3fac93599a6bf73ae
Elsa Hamon <elsa.hamon@etu.unicaen.fr>
2025-10-15 12:02:29 +0200

    fin du cours 1

--- src/server.js
+++ src/server.js
@@ -1,11 +1,32 @@
-import http from 'http'
+import { error } from 'console'
+import express from 'express'
 
 const PORT = process.env.PORT || 3000
 
-const server = http.createServer((req, res) => {
-    res.end('HeLL0 ClaSSE')
+const app = express()
+
+app.use(express.json())
+
+app.get('/questions', (req, res) => {
+    res.status(200).send([
+        {
+            id: "1",
+            question: "Quelle est la capitale de la Bulgarie ?",
+            answer: "Sofia"
+        }
+    ])
+
 })
 
-server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
+app.post('/questions', (req, res) => {
+    const { question, answer } = req.body
+
+    if(!question || !answer) {
+        return res.status(400).send({ error: "Invalid request" })
+    }
+    res.status(201).send({ message: "Question created" })
+})
+
+app.listen(PORT, () => {
+    console.log(`server is running on http://localhost:${PORT}`)
 })
